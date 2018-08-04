738c826c4926a427df3c8b6d1c47c08836468b5c
Thomas Wycichowski <wycichowski@live.de>
2018-08-04 23:37:12 +0200

    Version 1.0.0

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            XlfToClass();
+            ClassToXlf();
+            
+        }
+
+        static void XlfToClass()
+        {
+            string xml =
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                "<xliff version=\"1.2\" xmlns=\"urn:oasis:names:tc:xliff:document:1.2\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"urn:oasis:names:tc:xliff:document:1.2 xliff-core-1.2-transitional.xsd\">" +
+                    "<file datatype=\"xml\" source-language=\"en-US\" target-language=\"fr\" tool-id=\"UIStrings\">" +
+                        "<header>" +
+                            "<tool tool-id=\"Name\" tool-name=\"Name\" tool-company=\"TWyTec\"/>" +
+                        "</header>" +
+                        "<body>" +
+                            "<group datatype=\"resx\">" +
+                                "<trans-unit xmlns=\"\" id=\"Hi\">" +
+                                    "<source>Hello World</source>" +
+                                    "<target>Bonjour le monde</target>" +
+                                    "<note>Hello World</note>" +
+                                "</trans-unit>" +
+                            "</group>" +
+                        "</body>" +
+                    "</file>" +
+                "</xliff>";
+
+            string xmlWithoutGroup =
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                "<xliff version=\"1.2\" xmlns=\"urn:oasis:names:tc:xliff:document:1.2\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"urn:oasis:names:tc:xliff:document:1.2 xliff-core-1.2-transitional.xsd\">" +
+                    "<file datatype=\"xml\" source-language=\"en-US\" target-language=\"fr\" tool-id=\"UIStrings\">" +
+                        "<header>" +
+                            "<tool tool-id=\"Name\" tool-name=\"Name\" tool-company=\"TWyTec\"/>" +
+                        "</header>" +
+                        "<body>" +
+                            "<trans-unit xmlns=\"\" id=\"Hi\">" +
+                                    "<source>Hello World</source>" +
+                                    "<target>Bonjour le monde</target>" +
+                                    "<note>Hello World</note>" +
+                                "</trans-unit>" +
+                        "</body>" +
+                    "</file>" +
+                "</xliff>";
+
+            var model = XlfParser.Converter.Deserialize(xml);
+            var modelWithoutGroup = XlfParser.Converter.Deserialize(xmlWithoutGroup);
+        }
+
+        static void ClassToXlf()
+        {
+            XlfParser.Model.Xliff xliff = new XlfParser.Model.Xliff() {
+                Version = 1.2m,
+                File = new XlfParser.Model.File() {
+                    Datatype = "xml",
+                    SourceLanguage = "en-US",
+                    TargetLanguage = "fr-fr",
+                    ToolId = "Xlf",
+                    Header = new XlfParser.Model.Header() {
+                        Tool = new XlfParser.Model.Tool()
+                        {
+                            Id = "Xlf",
+                            Company = "TWyTec",
+                            Name = "Xlf"
+                        }
+                    },
+                    Body = new XlfParser.Model.Body() {
+                        TransUnit = new System.Collections.Generic.List<XlfParser.Model.TransUnit>()
+                        {
+                            new XlfParser.Model.TransUnit() { Id = "Hi", Note = "Hello World", Source = "Hello World", Target = "Bonjour le monde"}
+                        }
+                    }
+                }
+            };
+
+            var xml = XlfParser.Converter.Serialize(xliff);
+        }
+    }
+}
--- XlfParser/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace XlfParser
-{
-    public class Class1
-    {
-    }
-}
